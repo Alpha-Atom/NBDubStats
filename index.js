cce41de19938ec8acf51b4d92ffdd253b6411a77
Matt <mattc65@gmail.com>
2016-03-07 08:07:04 +0000

    Add basic functionality to ensure stats are being grabbed correctly

---
+++ index.js
@@ -0,0 +1,37 @@
+var DubAPI = require("dubapi");
+var userName = "DubStatsBot";
+var passWord = process.argv[2];
+
+new DubAPI({username: userName, password: passWord}, function (err, bot) {
+  if (err) return console.error(err);
+
+  console.log("Running DubAPI v" + bot.version);
+
+  function connect() {
+    bot.connect("nightblue3");
+  }
+
+  bot.on('connected', function(name) {
+    console.log("Successfully connected to " + name "!");
+  });
+
+  bot.on('disconnected', function(name) {
+    console.log("Unexpected disconnect, reconnecting...");
+    setTimeout(connect, 15000);
+  });
+
+  bot.on('error', function(err) {
+    console.error(err);
+  });
+
+  bot.on(bot.events.roomPlaylistUpdate, function(data) {
+     var lastplayed = data.lastPlay;
+     var score = lastplayed.score.updubs - lastplayed.score.downdubs;
+     var grabs = lastplayed.score.grabs;
+     var trackname = lastplayed.media.name;
+     var fkid = lastplayed.media.fkid;
+
+     console.log("Last played was track: " + trackname + " id: " + fkid);
+     console.log("Total score was: ": score + " also grabs: " + grabs);
+  });
+});
