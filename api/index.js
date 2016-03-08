6c49cb48d746bdf191dd37f66a655a2aac120416
Matt <mattc65@gmail.com>
2016-03-08 03:22:01 +0000

    version control is for losers

--- api/index.js
+++ api/index.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var app = express();
+var fs = require("fs");
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/dubstats/', function (req, res) {
+  var time1 = req.query.t1 || -1;
+  var time2 = req.query.t2 || -1;
+
+  json = fs.readFileSync("output.json");
+  songs = JSON.parse(json);
+  if (t1 == -1 && t2 == -1) {
+    res.send(json);
+  } else {
+    songs_only = 
+    Object.keys(obj).forEach(function (key) {
+      var timestamp = 
+    });
+  }
 });
 
 app.listen(3000, function () {
