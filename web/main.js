c021d74b5f09ca38caf6c31bda917e04947b0277
Matt <mattc65@gmail.com>
2016-03-08 06:44:32 +0000

    changes

--- web/main.js
+++ web/main.js
@@ -32,6 +32,8 @@ var update_sval = function () {
 };
 var populate_table = function(e) {
   e.preventDefault();
+  $("#table-body").html("");
+  showLoading();
   sval = update_sval();
   t1 = sval[0]-1;
   t2 = sval[1];
@@ -61,8 +63,47 @@ var populate_table = function(e) {
     t_slug = "t1=" + t1 + "&t2=" + t2
   }
   s_slug = "sort=" + sort;
-  total_slug = "mattcoles.io:3000/dubstats/?" + s_slug + ((t_slug != "") ? "&" + t_slug : "") + ((c_slug != "") ? "&" + c_slug : "");
-  console.log(total_slug);
+  total_slug = "http://mattcoles.io:3000/dubstats/?" + s_slug + ((t_slug != "") ? "&" + t_slug : "") + ((c_slug != "") ? "&" + c_slug : "");
+  $.getJSON(total_slug, function (data) {
+    song_list = data.songs;
+    console.log(song_list);
+    tr_list = [];
+    for (var i = 0; i < song_list.length; i++) {
+     var new_tr = document.createElement("tr");
+     var name_td = document.createElement("td");
+     name_td.innerHTML = song_list[i].song_name;
+     var total_td = document.createElement("td");
+     total_td.innerHTML = song_list[i].score;
+     var grabs_td = document.createElement("td");
+     grabs_td.innerHTML = song_list[i].grabs;
+     var plays_td = document.createElement("td");
+     plays_td.innerHTML = song_list[i].plays;
+     var pct_td = document.createElement("td");
+     pct_td.innerHTML = song_list[i].pct_up;
+     new_tr.appendChild(name_td);
+     new_tr.appendChild(total_td);
+     new_tr.appendChild(grabs_td);
+     new_tr.appendChild(plays_td);
+     new_tr.appendChild(pct_td);
+     tr_list.push(new_tr);
+    }
+    $("#table-body").hide();
+    for (var j = tr_list.length-1; i >= 0; i--) {
+      $("#table-body").append(tr_list[i]);
+    }
+    hideLoading();
+    $("#table-body").show();
+  });
   e.target.blur();
 }
+
+var showLoading = function () {
+  $(".spinner").show();
+}
+
+var hideLoading = function () { 
+  $(".spinner").hide();
+}
+
+hideLoading();
 update_sval();
